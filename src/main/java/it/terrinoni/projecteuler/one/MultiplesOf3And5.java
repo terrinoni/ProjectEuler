163c20ab984154b9fb5189af6e06ae39431248cf
Marco Terrinoni <marco.terrinoni90@gmail.com>
2015-07-14 18:33:31 +0200

    problem 1 solved

--- src/main/java/it/terrinoni/projecteuler/one/MultiplesOf3And5.java
+++ src/main/java/it/terrinoni/projecteuler/one/MultiplesOf3And5.java
@@ -24,9 +24,24 @@
 package it.terrinoni.projecteuler.one;
 
 /**
+ * Multiples of 3 and 5; problem 1.
  *
  * @author Marco Terrinoni
  */
 public class MultiplesOf3And5 {
-    
+
+    public static void main(String[] args) {
+        int limit = 1000,
+                div1 = 3,
+                div2 = 5,
+                sum = 0;
+        
+        for(int i = 3; i < limit; i++) {
+            if((i % div1 == 0) || (i % div2 == 0)) {
+                sum += i;
+            }
+        }
+        
+        System.out.println("The sum of all the multiples of 3 or 5 below 1000 is: " + sum);
+    }
 }
--- src/main/java/it/terrinoni/projecteuler/one/README.md
+++ src/main/java/it/terrinoni/projecteuler/one/README.md
@@ -5,4 +5,4 @@ The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 
-*Answer: x*
+*Answer: 233168*
